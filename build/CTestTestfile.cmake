f77d8467a30531dbb2fe2c0f587fd4c94b7fbe2a
IlliaFransua <illia.fransua@icloud.com>
2025-03-25 21:49:42 +0100

    update: remove build/ from .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/illiafransua/Desktop/Практика/file_Storage
-# Build directory: /Users/illiafransua/Desktop/Практика/file_Storage/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
