c6220d31d57b9aaa376ba0630875d31123e6376a
IlliaFransua <illia.fransua@icloud.com>
2025-03-25 21:33:50 +0100

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/illiafransua/Desktop/Практика/file_Storage
+# Build directory: /Users/illiafransua/Desktop/Практика/file_Storage/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
---
+++ build/src/CMakeFiles/file_storage.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/file_storage.dir/file_storage.c.o"
+  "CMakeFiles/file_storage.dir/file_storage.c.o.d"
+  "CMakeFiles/file_storage.dir/metadata.c.o"
+  "CMakeFiles/file_storage.dir/metadata.c.o.d"
+  "CMakeFiles/file_storage.dir/utils.c.o"
+  "CMakeFiles/file_storage.dir/utils.c.o.d"
+  "libfile_storage.a"
+  "libfile_storage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/file_storage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
